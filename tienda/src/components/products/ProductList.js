81a962b3f00b4ee48c6f963bbb0e0b7e033d5ceb
Barış Can Çifçi <17030220013@student.beykent.edu.tr>
2021-11-14 12:28:26 +0200

    commit
    api'den ürünleri fetch yapısı ile redux kullanarak getirdim. gelen dizi şeklindeki datayı map ile reactstrap card elementine geçirdim ve ürün filtreleme işlemini yaptım.

--- tienda/src/components/products/ProductList.js
+++ tienda/src/components/products/ProductList.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import {
+  Card,
+  CardBody,
+  CardImg,
+  CardTitle,
+  CardText,
+  Button,
+  CardSubtitle,
+} from "reactstrap";
 import { Badge } from "reactstrap";
+import { bindActionCreators } from "redux";
+import * as productActions from "../../redux/actions/productActions";
 
 class ProductList extends Component {
+  componentDidMount() {
+    this.props.actions.getProducts();
+  }
+
   render() {
     return (
       <div>
@@ -12,6 +27,24 @@ class ProductList extends Component {
             {this.props.currentCategory.categoryName}
           </Badge>
         </h3>
+        {this.props.products.map((product) => (
+          <Card key={product.id} style={{ width: 500 }}>
+            <CardImg
+              alt="Card image cap"
+              src={product.productImg}
+              top
+              width="50%"
+            />
+            <CardBody>
+              <CardTitle tag="h5">{product.productName}</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                {product.productDesc}
+              </CardSubtitle>
+              <CardText>{product.productPrice}TL</CardText>
+              <Button>Sipariş Ver</Button>
+            </CardBody>
+          </Card>
+        ))}
       </div>
     );
   }
@@ -19,7 +52,16 @@ class ProductList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
+    products: state.productListReducer,
   };
 }
 
-export default connect(mapStateToProps)(ProductList);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getProducts: bindActionCreators(productActions.getProducts, dispatch),
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
